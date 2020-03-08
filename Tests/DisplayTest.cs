424e74f7335db1182c873e38e4afb5735d5924a2
Adel Vilkov <vilkov.adel@gmail.com>
2020-03-08 21:11:00 +0300

    Updated docs, improved DisplayTest for corner cases, added more info to DisplayMode ToString()

--- Tests/DisplayTest.cs
+++ Tests/DisplayTest.cs
@@ -1,32 +1,44 @@
+using FluentAssertions;
 using Imagini;
 using Xunit;
 
 namespace Tests
 {
     [DisplayTestMethodName]
-    public class DisplayTest 
+    public class DisplayTest
     {
         [Fact]
         public void ShouldEnumerateDisplays()
         {
-            
+
             Assert.NotNull(Display.All);
 #if !HEADLESS
             Assert.NotEmpty(Display.All);
 #endif
-        }       
+        }
 
 #if !HEADLESS
         [Fact]
         public void ShouldEnumerateDisplayModes()
         {
-            
+
             foreach(var display in Display.All)
             {
                 Assert.NotEmpty(display.Modes);
                 Assert.NotNull(display.Name);
-                Assert.Contains(display.CurrentMode, display.Modes);
-                Assert.Contains(display.DesktopMode, display.Modes);
+                var currentMode = display.CurrentMode;
+                var desktopMode = display.DesktopMode;
+
+                /*
+                    Note: workaround for SDL sometimes reporting current/desktop
+                    mode index as -1 (ex. when launched in Xephyr)
+                */
+                display.Modes.Should().Contain(d =>
+                    d.Width == currentMode.Width &&
+                    d.Height == currentMode.Height);
+                display.Modes.Should().Contain(d =>
+                    d.Width == desktopMode.Width &&
+                    d.Height == desktopMode.Height);
             }
         }
     }
